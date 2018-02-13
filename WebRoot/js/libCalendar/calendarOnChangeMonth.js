4ae1c3025c60f4545db7bedd162b6c91bedf6c13
f.troccoli <f.troccoli@c81b5bb8-e782-4fe7-967c-ac36ce986653>
2018-02-13 16:00:59 +0000

    refs closes #11221 @0h10m

--- WebRoot/js/libCalendar/calendarOnChangeMonth.js
+++ WebRoot/js/libCalendar/calendarOnChangeMonth.js
@@ -9,6 +9,7 @@ function calendarOnChangeMonth(isnext)
 	var _year = this.date.getFullYear();
 	var _month = this.date.getMonth() + 1;
 	var _date = this.date.getDate();
+	var msg= "年份超出范围（1000-9999）!";
 	if(isnext){ _month= _month + 1;} else {_month= _month - 1;}
 	if(_year > 999 && _year <10000)
 	{ 
@@ -17,7 +18,7 @@ function calendarOnChangeMonth(isnext)
 	}
 	else
 	{
-		alert("年份超出范围（1000-9999）!");
+		alert(msg);
 	}  
 	this.bindDate(_year + '-' + _month + '-' + _date);
 }
--- WebRoot/js/libCalendar/calendarOnChangeYear.js
+++ WebRoot/js/libCalendar/calendarOnChangeYear.js
@@ -8,13 +8,14 @@ function calendarOnChangeYear(isnext)
 	var _year = this.date.getFullYear();
 	var _month = this.date.getMonth() + 1;
 	var _date = this.date.getDate();
+	var msg = "年份超出范围（1000-9999）!";
 	if(_year > 999 && _year <10000)
 	{
 		if(isnext){_year= _year + 1;}else{ _year = _year - 1;}
 	}
 	else
 	{
-		alert("年份超出范围（1000-9999）!");
+		alert(msg);
 	}
 	this.bindDate(_year + '-' + _month + '-' + _date);
 }
